a2db61f36ae5a06ccbdd9ad643a28a50821061ab
Martin Knoll <martinknoll@me.com>
2012-04-24 02:34:43 +0200

    Initial Commit

---
+++ OpenGLES/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OpenGLES
+//
+//  Created by Martin Knoll on 24.04.12.
+//  Copyright (c) 2012 Otto-von-Guericke-Universität Magdeburg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OpenGLES/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OpenGLES
+//
+//  Created by Martin Knoll on 24.04.12.
+//  Copyright (c) 2012 Otto-von-Guericke-Universität Magdeburg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
